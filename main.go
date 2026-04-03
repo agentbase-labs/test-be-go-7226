a8c8f2a8549efe97817c6e4e54d8ae30bb48174b
agentbase-labs <dev@agentbase.network>
2026-04-04 01:13:51 +0500

    Initial go deploy

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "10000"
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"message": "Go Works!"})
+	})
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"status": "ok", "framework": "go"})
+	})
+	fmt.Printf("Server on port %s\n", port)
+	http.ListenAndServe("0.0.0.0:"+port, nil)
+}
